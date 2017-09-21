1d3578f2995888119a4e8571e9dfb2d4e2a76176
gasten <g.sauzande@student.fontys.nl>
2017-09-21 14:09:14 +0200

    Fixing environment variables problem

--- db.js
+++ db.js
@@ -1,34 +1,12 @@
 require('dotenv').config();
-var request = require('request');
-var env_vars;
-var options = {
-  url: 'https://api.heroku.com/apps/gexlabeler/config-vars',
-  headers: {
-    'Accept': 'application/vnd.heroku+json; version=3',
-    'Authorization' : 'Bearer ad38aa1f-441c-48b7-9b72-4f10badc61f4'
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    port : process.env.DB_PORT,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME
   }
-};
-
-request(options, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  if(error == null && response.statusCode == 200){
-    env_vars = body;
-    console.log(env_vars);
-
-
-    var knex = require('knex')({
-      client: 'mysql',
-      connection: {
-        host : process.env.DB_HOST,
-        user : process.env.DB_USER,
-        port : process.env.DB_PORT,
-        password : process.env.DB_PASS,
-        database : process.env.DB_NAME
-      }
-    });
-    module.exports = knex;
-
-  }
-
 });
+module.exports = knex;
--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ var code = randomstring.generate(12);
 router.get('/', function(req, res, next) {
     res.render('../views/login', { title: 'login',code:code});
 });
-router.post('/', function(req, res, next) {
+router.post('/login', function(req, res, next) {
     console.log(JSON.stringify(req.body));
     knex('users').insert({username:req.body.name,code:code,points: 0}).then( function (result) {
           console.log(result);     // respond back to request
