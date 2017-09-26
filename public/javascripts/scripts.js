cce67f7cbace4ff87e0e92494b5c1c820fc83539
gasten <g.sauzande@student.fontys.nl>
2017-09-26 15:41:55 +0200

    Removed the dots on the labeling page

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,22 +1,5 @@
 $(document).ready(function(){
-  var hasFlip = false;
-  $( ".link" ).mouseenter(function() {
-    if(!hasFlip){
-      $(".link").addClass("flipInX");
-      hasFlip = true;
-    }
-  });
 
-  $( ".link" ).mouseleave(function() {
-    if(hasFlip){
-      $(".link").removeClass("flipInX");
-      hasFlip = false;
-    }
-  });
-
-//slide up the code
-//slide down the code input
-//change the register button to an arrow
 var reg = true;
 $("#register").click(function(){
   if(reg){
