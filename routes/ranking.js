f826ca3c4be4279cde43e61f3e62910985c4780c
gasten <g.sauzande@student.fontys.nl>
2017-09-27 10:48:45 +0200

    Removed annonymous functions from callbacks

--- routes/ranking.js
+++ routes/ranking.js
@@ -3,27 +3,30 @@ var router = express.Router();
 var knex = require('../db.js');
 
 router.get('/', function(req, res, next) {
+  
   knex.select('username','points')
     .from('users')
     .orderBy('points','desc')
     .on('query-error', handleError)
-    .then(function(data) {
+    .then(sendRankings);
+
+
+    function sendRankings(data) {
       res.render('../views/ranking', { title: 'Ranking',users : data});
-    });
+    }
+
+    function handleError(error, obj) {
+      if(req.app.get('env') === 'development'){
+        res.render('../views/error', {message: 'Sorry, our bad. This is not your fault...', error : error});
+      }else{
+        res.render('../views/error', {message: 'Sorry, our bad. This is not your fault...', error : {}});
+      }
+
+    }
+
 
 });
 
 
-function sendRankings(data) {
-  res.render('../views/ranking', { title: 'Ranking',users : data});
-}
 
-function handleError(error, obj) {
-  if(req.app.get('env') === 'development'){
-    res.render('../views/error', {message: 'Sorry, our bad. This is not your fault...', error : error});
-  }else{
-    res.render('../views/error', {message: 'Sorry, our bad. This is not your fault...', error : {}});
-  }
-
-}
 module.exports = router;
